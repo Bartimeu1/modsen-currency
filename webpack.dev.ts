30502b7a3f941637390b4810f01687546b3ff9f4
Ivan Sapego <sapego-ivan@list.ru>
2024-01-16 16:59:28 +0300

    fix: type for devServer webpack field

--- webpack.dev.ts
+++ webpack.dev.ts
@@ -1,8 +1,13 @@
 import path from 'path';
-import { Configuration } from 'webpack';
+import { Configuration as WebpackConfiguration } from 'webpack';
+import { Configuration as WebpackDevServerConfiguration } from 'webpack-dev-server';
 import { merge } from 'webpack-merge';
 
 import commonConfig from './webpack.common';
+
+interface Configuration extends WebpackConfiguration {
+  devServer: WebpackDevServerConfiguration;
+}
 
 const devConfig: Configuration = {
   mode: 'development',

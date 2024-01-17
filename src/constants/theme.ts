68f8baca46da486f6a8607512dd46244decc5262
Ivan Sapego <sapego-ivan@list.ru>
2024-01-17 16:28:58 +0300

    feat: added theme constants file

---
+++ src/constants/theme.ts
@@ -0,0 +1,66 @@
+const lightTheme = {
+  background: '#030304',
+  border: '#474747',
+  text: '#fff',
+  toggleButton: '#fff',
+  inputBg: '#1B2028',
+  title: 'linear-gradient(90deg, #00CE2C 0.18%, #AEDF23 49.3%, #A3DC00 99.88%)',
+} as const;
+
+const darkTheme = {
+  background: '#f5f5f5',
+  border: '#030304',
+  text: '#000',
+  toggleButton: '#000',
+  inputBg: '#fff',
+  title: 'linear-gradient(90deg, #00CE2C 0.18%, #AEDF23 49.3%, #A3DC00 99.88%)',
+} as const;
+
+const layoutValues = {
+  fontSize: {
+    xs3: '12px',
+    xs2: '20px',
+    xs1: '24px',
+    sm: '25px',
+    md: '26px',
+    lg: '30px',
+    xl1: '32px',
+    xl2: '33px',
+    xl3: '35px',
+    xl4: '38px',
+    xl5: '90px',
+  },
+  fontWeight: {
+    light: 300,
+    regular: 400,
+    medium: 500,
+    semiBold: 600,
+    bold: 700,
+  },
+  borderRadius: {
+    small: '8px',
+    medium: '30px',
+    circle: '50%',
+  },
+  spaces: {
+    xs3: '4px',
+    xs2: '8px',
+    xs: '16px',
+    sm: '24px',
+    md: '31px',
+    lg: '56px',
+  },
+} as const;
+
+const theme = {
+  dark: {
+    color: darkTheme,
+    ...layoutValues,
+  },
+  light: {
+    color: lightTheme,
+    ...layoutValues,
+  },
+} as const;
+
+export default theme;

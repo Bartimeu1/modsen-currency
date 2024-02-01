cb3d607df3bb992b6f5b06b2da279f85afb56180
Ivan Sapego <sapego-ivan@list.ru>
2024-02-01 04:18:01 +0300

    fix: added create root instead of ReactDOM render

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 
-import { persistor,store } from '@store/store';
+import { persistor, store } from '@store/store';
 import { PersistGate } from 'redux-persist/integration/react';
 
 import App from './App';
 
-ReactDOM.render(
+const root = createRoot(document.getElementById('root') as Element);
+
+root.render(
   <Provider store={store}>
     <PersistGate persistor={persistor}>
       <App />
     </PersistGate>
   </Provider>,
-  document.getElementById('root') as HTMLElement,
 );

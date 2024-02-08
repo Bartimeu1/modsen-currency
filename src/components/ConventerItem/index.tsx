09d23bb35d7a7b19d490de6975df69f8927fa0e8
Ivan Sapego <sapego-ivan@list.ru>
2024-02-08 19:44:44 +0300

    feat: added mock data to cypress tests

--- src/components/ConventerItem/index.tsx
+++ src/components/ConventerItem/index.tsx
@@ -21,9 +21,9 @@ export const ConverterItem = memo(function ConverterItem(
     <StyledConverterItem
       onClick={onClick(currencyCode)}
       data-testid="converter-item">
-      <IconImage src={image} />
+      <IconImage src={image} data-testid="converter-item-image" />
       <Content>
-        <Title>{title}</Title>
+        <Title data-testid="converter-item-title">{title}</Title>
         <Rate>R$ {convertAndFormatCurrencyData(rate)}</Rate>
       </Content>
     </StyledConverterItem>
